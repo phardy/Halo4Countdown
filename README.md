0001314c66af084e6bcdbb38fc78a2585feb358a
Marcin Szczepanski <marcin@polishedmonocle.com>
2012-09-26 18:25:29 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Just a silly little Javascript experiment I did on the train on the way home from work.
+
+Inspired by: [http://www.reddit.com/r/gaming/comments/10gxc2/so_these_just_arrived_today/c6dhrit]
---
+++ javascripts/countdown.js
@@ -0,0 +1,91 @@
+(function(Countdown, $, undefined) {
+    //Nov 6, 2012 12:01 am
+
+    var targetTime = new Date(2012, 10, 6, 0, 1),
+
+    // digits 0 through 9
+    segments = [
+            ["t", "tl", "bl", "b", "br", "tr"],
+            ["tr", "br"],
+            ["t", "tr", "m", "bl", "b"],
+            ["t", "tr", "m", "br", "b"],
+            ["tl", "m", "tr", "br"],
+            ["t", "tl", "m", "br", "b"],
+            ["t", "tl", "m", "br", "b", "bl"],
+            ["t", "tr", "br"],
+            ["t", "tl", "bl", "b", "br", "tr", "m"],
+            ["t", "tl", "b", "br", "tr", "m"]
+        ],
+        $container, digitWidth = 25;
+
+    $(function() {
+        $container = $(".timer");
+        setInterval(Countdown.refreshTimer, 1000);
+        Countdown.refreshTimer();
+    });
+
+    Countdown.refreshTimer = function ()
+    {
+        $container.find(".digit").remove();
+        var timeRemaining = (targetTime.getTime() - new Date().getTime()) / 1000;
+        var daysRemaining = 0;
+        var hoursRemaining = 0;
+        var minutesRemaining = 0;
+        var secondsRemaining = 0;
+        if (timeRemaining > 0)
+        {
+            daysRemaining = Math.floor(timeRemaining / (24 * 60 * 60));
+            timeRemaining -= (daysRemaining * 24 * 60 * 60);
+            hoursRemaining = Math.floor(timeRemaining / (60 * 60));
+            timeRemaining -= hoursRemaining * 60 * 60;
+            minutesRemaining = Math.floor(timeRemaining / 60);
+            timeRemaining -= minutesRemaining * 60;
+            secondsRemaining = Math.floor(timeRemaining);
+        }
+
+//        console.log(daysRemaining,hoursRemaining,minutesRemaining,secondsRemaining);
+
+        Countdown.drawNumberString(frontPad(daysRemaining, 4), 5, 5);
+        Countdown.drawNumberString(frontPad(hoursRemaining, 2), 132, 2);
+        Countdown.drawNumberString(frontPad(minutesRemaining, 2), 212, 1);
+        Countdown.drawNumberString(frontPad(secondsRemaining, 2), 291, 0);
+    };
+
+    frontPad = function (number, digits)
+    {
+        var numberString = "" + number;
+        var len = numberString.length;
+        if (len < digits)
+        {
+            for (var i = 0; i < digits - len; i++)
+            {
+                numberString = "0" + numberString;
+            }
+        }
+        return numberString;
+    };
+
+    Countdown.drawNumberString = function (numberString, x, y)
+    {
+        var len = numberString.length;
+        for (var i = 0; i < len; i++)
+        {
+            var c = numberString.charCodeAt(i);
+            var digit = c - "0".charCodeAt(0);
+            Countdown.drawDigit(digit, x, y);
+            x += digitWidth;
+        }
+    };
+
+    Countdown.drawDigit = function (digit, x, y) {
+        var $digit = $("<div class='digit'></div>");
+        $.each(segments[digit], function () {
+            var $segment = $("<div></div>");
+            $segment.addClass("segment-" + this);
+            $digit.append($segment);
+        });
+        $container.append($digit);
+        $digit.css({"left": x, "top": y});
+    };
+
+})(window.Countdown = window.Countdown || {}, jQuery);
